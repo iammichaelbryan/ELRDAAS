f547da8fd5596512f79f6c3f658cc3a9fb3cd1a1
Michael Bryan <mikeybryan2310@gmail.com>
2023-11-30 00:13:38 -0500

    Increment with Dark Mode, notices, notifications and View Laundromat Appointments

---
+++ notifications.js
@@ -0,0 +1,10 @@
+function showNotification() {
+    var notification = document.getElementById("notification");
+    notification.style.display = 'block';
+    //document.getElementById("notification").innerHtml = notification;
+}
+
+function hideNotification() {
+    var notification = document.getElementById('notification');
+    notification.style.display = 'none';
+}
