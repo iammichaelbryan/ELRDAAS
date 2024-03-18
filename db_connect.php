dc5f1d483a93d1c39837bab56a795844fff0d4f9
Michael G. Bryan III <mikeybryan2310@gmail.com>
2024-03-18 14:15:49 -0500

    Notice Board with no script implemented

--- db_connect.php
+++ db_connect.php
@@ -10,7 +10,6 @@ try {
     $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
     // Set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Connected successfully";
 } catch(PDOException $e) {
     echo "Connection failed: " . $e->getMessage();
 }
